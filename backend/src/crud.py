954b5e72a00fe81184c593b43065a9dd158d921a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-12-14 08:26:31 +0900

    Add read_user function

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 import datetime
+
+from sqlalchemy.sql.functions import user
 from models import History, User, UsersHistory
 
 def create_history(db:Session, spotify_song_id:str):
@@ -23,6 +26,12 @@ def create_user(db:Session, username:str, password:str):
 	db.commit()
 	db.refresh(db_user)
 	return db_user
+
+def read_user(db:Session, username:str):
+	statement = select(User).filter_by(username=username)
+	result = db.execute(statement).first()._asdict()['User']
+
+	return result
 
 def create_users_history(db:Session, user_id:int, spotify_song_id:str):
 	db_users_history = UsersHistory(user_id=user_id, spotify_song_id=spotify_song_id, created_at=datetime.datetime.now())
