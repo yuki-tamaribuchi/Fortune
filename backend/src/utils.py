2f948b016461850c9832b3ae98270d4922f758e7
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-12-15 08:54:26 +0900

    Decouple to clac_hash and hash_password

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,14 +1,19 @@
-def hash_password(password:str):
+def calc_hash(password:str, salt:bytes):
 	import hashlib
-	import os
 
-
-	salt = os.urandom(32)
 	key = hashlib.pbkdf2_hmac(
 		'sha256',
 		password.encode('utf-8'),
 		salt,
 		100000
 	)
+
+	return key
+
+def hash_password(password:str):
+	import os
+
+	salt = os.urandom(32)
+	key = calc_hash(password, salt)
 
 	return salt.hex(), key.hex()
