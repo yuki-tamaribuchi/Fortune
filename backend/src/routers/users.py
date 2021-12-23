917fdb88ea4f44da6f711c953eb9d67fc0afac9e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-12-24 00:05:24 +0900

    Move functions from main

---
+++ backend/src/routers/users.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from dependencies import get_db
+from crud import create_user, read_user, delete_user
+from schemas import UserCreate, UserLogin, UserDelete
+from auth_handler import signJWT, decodeJWT, authentication
+from auth_bearer import JWTBearer
+
+router = APIRouter()
+
+@router.post("/register/")
+async def register_user(user:UserCreate, db:Session=Depends(get_db)):
+	user_instance = create_user(db=db, username=user.username, password=user.password)
+	if user_instance:
+		return signJWT(user_instance.username)
+	return HTTPException(409)
+
+
+@router.post("/login/")
+async def login_user(user:UserLogin, db:Session=Depends(get_db)):
+	user_instance = authentication(db=db, user=user)
+	if user_instance:
+		return signJWT(user_instance.username)
+	raise HTTPException(401)
+
+
+@router.get("/users/")
+async def myself_read(db:Session=Depends(get_db), jwt_payload=Depends(JWTBearer())):
+	decoded_payload = decodeJWT(jwt_payload)
+	user_instance = read_user(db=db, username=decoded_payload['username'])
+	if user_instance:
+		return user_instance
+	else:
+		raise HTTPException(404)
+
+
+@router.get("/users/{username}")
+async def user_read(username:str , db:Session=Depends(get_db)):
+	user_instance = read_user(db=db, username=username)
+	if user_instance:
+		return user_instance
+	else:
+		raise HTTPException(404)
+
+
+@router.delete("/users", status_code=status.HTTP_200_OK)
+async def user_delete(user:UserDelete, db:Session=Depends(get_db)):
+	user_instance = authentication(db, user)
+	if user_instance:
+		delete_user(db, user_instance)
+	else:
+		raise HTTPException(401)
