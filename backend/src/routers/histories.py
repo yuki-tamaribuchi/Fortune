2285eddd7ff9d0e06b81cdb6f4273857da1ac47a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-12-30 10:03:26 +0900

    Create histories cruds

---
+++ backend/src/routers/histories.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from auth_handler import decodeJWT
+from auth_bearer import JWTBearer
+from dependencies import get_db
+from cruds.histories import create_history, create_users_history
+from cruds.users import read_user
+from spotify import get_track_from_spotify
+
+router = APIRouter()
+
+@router.get("/tracks/recommend")
+def read_fortune_track(db:Session=Depends(get_db)):
+	data = get_track_from_spotify()
+	create_history(db=db, spotify_song_id=data['id'])
+	return data
+
+
+@router.get("/tracks/recommend/authed")
+def read_fortune_track(db:Session=Depends(get_db), jwt_payload=Depends(JWTBearer())):
+	data = get_track_from_spotify()
+	decoded_payload = decodeJWT(jwt_payload)
+	user_instance = read_user(db=db, username=decoded_payload['username'])
+	create_users_history(db=db, user_id=user_instance.id, spotify_song_id=data['id'])
+	return data
