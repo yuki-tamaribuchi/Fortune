03ffeb30df540a01aae7e2eea97bbafbe0945f3f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-04 10:19:55 +0900

    Modify save_image function

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,3 +1,6 @@
+from os import environ
+
+
 def calc_hash(password:str, salt:bytes):
 	import hashlib
 
@@ -19,8 +22,24 @@ def hash_password(password:str):
 	return salt.hex(), key.hex()
 
 
-def save_image(image_object, path, filename):
+def save_image(image_object, path_and_filename):
 	import boto3
+	from botocore.exceptions import ClientError
+	import os
+	import logging
 
-	s3_client = boto3.client('s3')
+
+	s3_client = boto3.client(
+		's3',
+		aws_access_key_id=os.environ.get('AWS_S3_SECRET_ID'),
+		aws_secret_access_key=os.environ.get('AWS_S3_SECRET_KEY')
+	)
+
+	try:
+		response = s3_client.upload_fileobj(image_object, os.environ.get('AWS_S3_BUCKET'), path_and_filename)
+	except ClientError as e:
+		logging.error(e)
+		return False
+	return True
+
 	
