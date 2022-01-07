e6b9381c808a5570ff55f8dc8c337d0933cc6b77
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-07 09:30:13 +0900

    Modify to async

--- backend/src/routers/histories.py
+++ backend/src/routers/histories.py
@@ -11,15 +11,15 @@ from spotify import get_track_from_spotify
 router = APIRouter()
 
 @router.get("/tracks/recommend")
-def read_fortune_track(db:Session=Depends(get_db)):
-	data = get_track_from_spotify()
+async def read_fortune_track(db:Session=Depends(get_db)):
+	data = await get_track_from_spotify()
 	create_history(db=db, spotify_song_id=data['id'])
 	return data
 
 
 @router.get("/tracks/recommend/authed")
-def read_fortune_track(db:Session=Depends(get_db), jwt_payload=Depends(JWTBearer())):
-	data = get_track_from_spotify()
+async def read_fortune_track(db:Session=Depends(get_db), jwt_payload=Depends(JWTBearer())):
+	data = await get_track_from_spotify()
 	decoded_payload = decodeJWT(jwt_payload)
 	user_instance = read_user(db=db, username=decoded_payload['username'])
 	create_users_history(db=db, user_id=user_instance.id, spotify_song_id=data['id'])
